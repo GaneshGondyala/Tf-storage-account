6d1caec76e35c2f653125dc525c30d4afe8016a8
GaneshGondyala <139107137+GaneshGondyala@users.noreply.github.com>
2024-07-05 11:41:05 +0530

    Update storageaccount.tf

--- Terraform-manifest/storageaccount.tf
+++ Terraform-manifest/storageaccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "main" {
-  name = "bhanuwebappstorage"
+  name = "ganeshwebappstorage"
   resource_group_name = azurerm_resource_group.main.name
   location = azurerm_resource_group.main.location
   account_tier = "Standard"
